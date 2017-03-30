a373085a512fe54c106661b3bea466e2b6a7ff40
pavlpaks <paulsnpakson@gmail.com>
2017-03-31 01:38:04 +0800

    Create Model folder create initDB.java

---
+++ src/initDB.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Jake on 31/03/2017.
+ */
+import java.sql.*
+public class initDB {
+    public static void main(String args[]){
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+
+        }
+    }
+}
