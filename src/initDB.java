28eca2ae8dd404514a8f4d09e3afa618cf8aab77
pavlpaks <paulsnpakson@gmail.com>
2017-03-31 02:38:36 +0800

    remove sql inspection

--- src/initDB.java
+++ src/initDB.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class initDB {
     public static void main(String args[]){
         Connection c;
-        Statement stmt = null;
+        Statement stmt;
 
         try {
             Class.forName("org.sqlite.JDBC");
@@ -10,6 +10,25 @@ public class initDB {
 
             System.out.println("Database opened");
             String sql;
+
+            stmt = c.createStatement();
+
+            sql = "CREATE TABLE PLAYERS" +
+                    "(PLAYER_ID   INT PRIMARY KEY NOT NULL," +
+                    "PLAYER_NAME  TEXT            NOT NULL)";
+            stmt.executeUpdate(sql);
+            sql =  "CREATE TABLE GAMES" +
+                    "(GAME_ID     INT PRIMARY KEY NOT NULL," +
+                    "GAME_NAME    TEXT            NOT NULL)";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE PLAYER_GAME" +
+                    "(P_ID        INT," +
+                    " G_ID         INT," +
+                    " FOREIGN KEY(P_ID) REFERENCES PLAYERS(PLAYER_ID)," +
+                    " FOREIGN KEY(G_ID) REFERENCES GAMES(GAME_ID))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
 
         }  catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
