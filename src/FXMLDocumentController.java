cb95dfdad84900d5f7832358527f304c9a7a4272
Leolar May E. Hora <lehora@up.edu.ph>
2017-03-31 02:55:45 +0800

    hotfix begin

---
+++ src/FXMLDocumentController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication4;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author L-LHora
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException{
+        Stage stage; 
+        Parent root;
+        if(event.getSource()==button){
+           //get reference to the button's stage         
+           stage=(Stage) button.getScene().getWindow();
+           //load up OTHER FXML document
+     root = FXMLLoader.load(getClass().getResource("choosePlayerScene.fxml"));
+        //create a new scene with root and set the stage
+         Scene scene = new Scene(root);
+         stage.setScene(scene);
+         stage.show();
+       }
+
+    
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+}
