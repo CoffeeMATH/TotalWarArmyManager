08b5b3d542f2d36eac11a3a4f92d6faf66bfd10b
pavlpaks <paulsnpakson@gmail.com>
2017-03-31 02:11:49 +0800

    Make initDB work

--- src/initDB.java
+++ src/initDB.java
@@ -1,15 +1,19 @@
-/**
- * Created by Jake on 31/03/2017.
- */
-import java.sql.*
+import java.sql.*;
 public class initDB {
     public static void main(String args[]){
-        Connection c = null;
+        Connection c;
         Statement stmt = null;
 
         try {
             Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:TWAMDatabase.db");
 
+            System.out.println("Database opened");
+            String sql;
+
+        }  catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
         }
     }
 }
