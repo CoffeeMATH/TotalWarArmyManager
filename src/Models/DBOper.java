f65f803c00f61ced6e8159cab106cf16dfe7fc74
Leolar May E. Hora <lehora@up.edu.ph>
2017-04-01 14:27:17 +0800

    Create DBOper class for database operations Add new syntax for creating an Army table and its assoc. class with Games table

---
+++ src/Models/DBOper.java
@@ -0,0 +1,7 @@
+package Models;
+
+/**
+ * Created by L-LHora on 4/1/2017.
+ */
+public class DBOper {
+}
--- src/Models/initDB.java
+++ src/Models/initDB.java
@@ -17,20 +17,30 @@ public class initDB {
 
             sql = "CREATE TABLE PLAYERS" +
                     "(PLAYER_ID   INT PRIMARY KEY NOT NULL," +
-                    "PLAYER_NAME  TEXT            NOT NULL)";
+                    " PLAYER_NAME  TEXT            NOT NULL)";
             stmt.executeUpdate(sql);
             sql =  "CREATE TABLE GAMES" +
                     "(GAME_ID     INT PRIMARY KEY NOT NULL," +
-                    "GAME_NAME    TEXT            NOT NULL)";
+                    " GAME_NAME   TEXT            NOT NULL)";
             stmt.executeUpdate(sql);
             sql = "CREATE TABLE PLAYER_GAME" +
                     "(P_ID        INT," +
-                    " G_ID         INT," +
+                    " G_ID        INT," +
                     " FOREIGN KEY(P_ID) REFERENCES PLAYERS(PLAYER_ID)," +
                     " FOREIGN KEY(G_ID) REFERENCES GAMES(GAME_ID))";
             stmt.executeUpdate(sql);
-            sql = "CREATE TABLE ARMY";
+            sql = "CREATE TABLE ARMY" +
+                    "(ARMY_ID     INT," +
+                    " ARMY_NAME   TEXT," +
+                    " TYPE        INT)"; //0 for Land army and 1 for Navy
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE GAME_ARMY" +
+                    "(G_ID        INT," +
+                    " A_ID        INT," +
+                    " FOREIGN KEY(G_ID) REFERENCES GAMES(GAME_ID)," +
+                    " FOREIGN KEY(A_ID) REFERENCES ARMY(ARMY_ID))";
             stmt.close();
+
             c.close();
 
         }  catch ( Exception e ) {
