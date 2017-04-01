d9d464e2b0f0f2bbdb8e9a91dbfe895ce34db826
pavlpaks <paulsnpakson@gmail.com>
2017-04-01 15:53:45 +0800

    Create Unit Model

---
+++ src/Models/Unit.java
@@ -0,0 +1,11 @@
+package Models;
+
+/**
+ * Created by Paul on 31/03/2017.
+ */
+public class Unit {
+    public String u_name;
+    public int u_RCost;
+    public int u_UCost;
+    public int u_type;
+}
