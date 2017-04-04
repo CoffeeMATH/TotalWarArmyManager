f9a3c5e75580893fbf0029b2e3343680a2e42bf6
John Aaron Alcoseba <jqalcoseba@up.edu.ph>
2017-04-04 21:48:13 +0800

    Back to zero UI for ChoosePlayerScene.

--- src/com/coffeemath/totalwararmymanager/Controller/ChoosePlayerSceneController.java
+++ src/com/coffeemath/totalwararmymanager/Controller/ChoosePlayerSceneController.java
@@ -14,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -47,7 +48,7 @@ public class ChoosePlayerSceneController implements Initializable {
                         Player player = getTableView().getItems().get(getIndex());
                         Button playerBtn = new Button(player.getName());
                         playerBtn.setOnAction(e -> System.out.println(player.getName()));
-                        setGraphic(playerBtn);
+                        setGraphic();
                         setText(null);
                     }
                 }
@@ -74,44 +75,50 @@ public class ChoosePlayerSceneController implements Initializable {
                 }
             };
         });*/
-
-        TableColumn playerCol = new TableColumn();
-        playerCol.setCellValueFactory(new PropertyValueFactory<>("playerBtn"));
-        TableColumn editCol = new TableColumn();
-        editCol.setCellValueFactory(new PropertyValueFactory<>("editBtn"));
-
-        TableView playerList = new TableView();
-        playerList.setItems(loadPlayers());
-        playerList.getColumns().addAll(playerCol,editCol);
-
+        GraphicTable players = new GraphicTable(new PlayerBtn(), new EditBtn());
+        players.setItems(loadPlayers());
+        //TableView playerList = new TableView();
+        //playerList.setItems(loadPlayers());
+        //playerList.getColumns().addAll(playerCol,editCol);
         Pane h = new Pane();
         h.getChildren().add(new Button("Add Player"));
-        vBox.getChildren().addAll(playerList,h);
-        TableColumn delCol = new TableColumn();
-
+        vBox.getChildren().addAll(h);
     }
 
-    private ObservableList<PlayerRow> loadPlayers(){
-        ObservableList<PlayerRow> players = FXCollections.observableArrayList();
-        players.add(new PlayerRow("Aaron"));
+    private ObservableList<Player> loadPlayers(){
+        ObservableList<Player> players = FXCollections.observableArrayList();
+        players.add(new Player("Aaron"));
         return players;
     }
 }
 
-class PlayerRow{
-    private SimpleObjectProperty<Button> playerBtn;
-    private SimpleObjectProperty<Button> editBtn;
-    public PlayerRow(Player player){
-        playerBtn = new SimpleObjectProperty<Button>(new Button(player.getName()));
-        editBtn = new SimpleObjectProperty<Button>(new Button("Edit"));
+
+
+class PlayerBtn{
+    private Button playerBtn;
+    public PlayerBtn(Player player){
+        playerBtn = new Button(player.getName());
+        playerBtn.setOnAction(e -> System.out.println(playerBtn.getText()));
     }
-    public PlayerRow(String player){
-        this(new Player(player));
+    public PlayerBtn(){
+        this(new Player("Default"));
     }
 }
 
+class EditBtn{
+    private Player player;
+    private Button editBtn;
+    public EditBtn(Player player){
+        this.player = player;
+        editBtn = new Button("Edit");
+        editBtn.setOnAction(e -> System.out.println());
+    }
+    public EditBtn(){
+        this(new Player("Default"));
+    }
+}
 
-class Player{
+class Player extends Object{
     private String name;
     public Player(String name){
         this.name = name;
