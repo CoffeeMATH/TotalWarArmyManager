0774dd192ecd4f263385d07bc5dde56e9f91be70
John Aaron Alcoseba <jqalcoseba@up.edu.ph>
2017-04-05 11:10:31 +0800

    Changed ChoosePlayerScene UI layout.

--- src/com/coffeemath/totalwararmymanager/Controller/ChoosePlayerSceneController.java
+++ src/com/coffeemath/totalwararmymanager/Controller/ChoosePlayerSceneController.java
@@ -6,25 +6,19 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 public class ChoosePlayerSceneController implements Initializable {
 
@@ -33,18 +27,18 @@ public class ChoosePlayerSceneController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        /* Player Column */
         TableColumn<Player,String> playerCol = new TableColumn<>();
         playerCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        playerCol.setCellFactory(param -> {return new PlayerCell<>();});
-
+        playerCol.setCellFactory(param -> new PlayerCell<>());
+        /* Edit Column */
         TableColumn<Player,String> editCol = new TableColumn<>();
         editCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        editCol.setCellFactory(param -> {return new EditCell<>();});
-
+        editCol.setCellFactory(param -> new EditCell<>());
+        /* Delete Column */
         TableColumn<Player,String> delCol = new TableColumn<>();
         delCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        delCol.setCellFactory(param -> {return new DeleteCell<>();});
+        delCol.setCellFactory(param -> new DeleteCell<>());
 
         TableView<Player> players = new TableView<>();
         players.setItems(loadPlayers());
