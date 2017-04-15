90da6fbb01c132a12fd0715752e5d1af2f92b029
John Aaron Alcoseba <jqalcoseba@up.edu.ph>
2017-04-16 03:47:40 +0800

    Minor bindings set.

--- src/com/coffeemath/totalwararmymanager/Controller/Toolkit/GraphicCell.java
+++ src/com/coffeemath/totalwararmymanager/Controller/Toolkit/GraphicCell.java
@@ -1,5 +1,7 @@
 package com.coffeemath.totalwararmymanager.Controller.Toolkit;
 
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 
 /**
@@ -7,14 +9,18 @@ import javafx.scene.control.TableCell;
  */
 public class GraphicCell<S,T> extends TableCell<S,T>{
     private GraphicAction<T> graphicAction;
+
     public GraphicCell(GraphicAction<T> graphicAction){
         this.graphicAction = graphicAction;
     }
     @Override
     protected void updateItem(T item, boolean empty) {
         super.updateItem(item, empty);
-        if(!empty)
-            setGraphic(graphicAction.nodeGraphic(item));
+        if(!empty) {
+            Node graphic = graphicAction.nodeGraphic(item);
+            if(graphic instanceof Button) ((Button) graphic).prefWidthProperty().bind(getTableColumn().maxWidthProperty());
+            setGraphic(graphic);
+        }
         else
             setGraphic(null);
         setText(null);
