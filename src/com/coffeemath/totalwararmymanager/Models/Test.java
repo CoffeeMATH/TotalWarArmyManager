533e1a7f9d346c2373531c75fb9e8f89939360e8
John Michael <jnadera@up.edu.ph>
2017-04-17 03:19:36 +0800

    create test class

--- src/com/coffeemath/totalwararmymanager/Models/Test.java
+++ src/com/coffeemath/totalwararmymanager/Models/Test.java
@@ -4,8 +4,10 @@ package com.coffeemath.totalwararmymanager.Models;
  * Created by John Michael on 4/17/2017.
  */
 public class Test {
-    public static void main(){
-
+    public static void main(String[] args){
+        Players player = new Players();
+        System.out.println(player.PlayerList.get(0).getId());
+        System.out.println(player.PlayerList.get(0).getName());
     }
 
 }
